014536a0e69020eaec09f5dd7e180908f1628c29
u18886984 <u18886984@sib-wsm-0000207.sigma.sbrf.ru>
2021-02-15 01:18:36 +0700

    initial

---
+++ blog/urls.py
@@ -0,0 +1,18 @@
+# blog/urls.py
+#from django.urls import path
+
+#from .views import BlogListView
+
+#urlpatterns = [
+   # path('', BlogListView.as_view(), name='home'),
+#]
+
+# blog/urls.py
+from django.urls import path
+
+from .views import BlogListView, BlogDetailView  # новое изменение
+
+urlpatterns = [
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),  # новое изменение
+    path('', BlogListView.as_view(), name='home'),
+]
